8b3ab3ca084231fb98b4c351e9b7acba3e275902
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-05-20 22:22:32 +0700

    [ ^ ] Update App, Product, About

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Main from "./Pages/Main";
 import Home from "./Pages/Home";
-import About from "./Pages/Home";
 import SignIn from "./Pages/Auth/SignIn";
 import SignUp from "./Pages/Auth/SignUp";
 import ForgotPassword from "./Pages/Auth/ForgotPassword";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./Pages/404/NotFound";
+import About from "./Pages/About";
+import Product from "./Components/Product";
 
 function App() {
   return (
@@ -16,12 +17,15 @@ function App() {
           {/* HOME */}
           <Route path="/" element={<Main />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
 
           {/* AUTH */}
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Forgot" element={<ForgotPassword />} />
+
+          {/* NAV LINK */}
+          <Route path="/product" element={<Product />} />
+          <Route path="/about" element={<About />} />
 
           {/* 404 */}
           <Route path="/404" element={<NotFound />} />
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,11 +1,76 @@
 import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 const Product = () => {
   return (
     <>
-      <div>
-        <h1>Test</h1>
+      <Nav />
+      {/* Produk Baru */}
+      <div className="font-medium text-lg px-20 pt-10">
+        <h1>Produk Baru</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="max-w-xs rounded-lg overflow-hidden shadow-lg border border-gray-200 p-4 bg-white">
+            <img
+              src="src/Assets/Product/Basreng.png"
+              alt=""
+              className="w-full h-48 object-cover rounded-lg"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">Basreng Pedas</h3>
+              <p className="text-gray-700 text-lg">Rp. 12.000</p>
+              <p className="text-gray-500 text-sm mt-2">
+                200 pcs terjual dalam sehari
+              </p>
+            </div>
+          </div>
+          <div className="max-w-xs rounded-lg overflow-hidden shadow-lg border border-gray-200 p-4 bg-white">
+            <img
+              src="src/Assets/Product/Gemlong-Pedas.png"
+              alt=""
+              className="w-full h-48 object-cover rounded-lg"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">Gemlong Pedas</h3>
+              <p className="text-gray-700 text-lg">Rp. 15.000</p>
+              <p className="text-gray-500 text-sm mt-2">
+                230 pcs terjual dalam sehari
+              </p>
+            </div>
+          </div>
+          <div className="max-w-xs rounded-lg overflow-hidden shadow-lg border border-gray-200 p-4 bg-white">
+            <img
+              src="src/Assets/Product/Singkong-Original.png"
+              alt=""
+              className="w-full h-48 object-cover rounded-lg"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">
+                Keripik Singkong Original
+              </h3>
+              <p className="text-gray-700 text-lg">Rp. 16.000</p>
+              <p className="text-gray-500 text-sm mt-2">
+                100 pcs terjual dalam sehari
+              </p>
+            </div>
+          </div>
+          <div className="max-w-xs rounded-lg overflow-hidden shadow-lg border border-gray-200 p-4 bg-white">
+            <img
+              src="src/Assets/Product/Donat.png"
+              alt=""
+              className="w-full h-48 object-cover rounded-lg"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">Donat</h3>
+              <p className="text-gray-700 text-lg">Rp. 10.000</p>
+              <p className="text-gray-500 text-sm mt-2">
+                85 pcs terjual dalam sehari
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
