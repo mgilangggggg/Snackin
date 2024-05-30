87204ce0e9f3f098ae20183778da7046575c42b8
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-05-30 11:51:59 +0700

    [ ^ ] update DetailProduct

---
+++ src/Pages/DetailProduct.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import UserNavbar from "../Components/UserNavbar";
+import UserFooter from "../Components/UserFooter";
+
+const DetailProduct = () => {
+  return (
+    <>
+      {/* Navbar */}
+      <UserNavbar />
+      {/* End Navbar */}
+
+      {/* Footer */}
+      <UserFooter />
+      {/* End Footer */}
+    </>
+  );
+};
+
+export default DetailProduct;
