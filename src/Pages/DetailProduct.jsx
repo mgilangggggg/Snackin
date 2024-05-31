0cf3e6aa793687bb4699e9ae482557a68ceb14d6
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-05-31 10:59:02 +0700

    [ ^ ] update DetailProduct

--- src/Pages/DetailProduct.jsx
+++ src/Pages/DetailProduct.jsx
@@ -3,6 +3,9 @@ import UserNavbar from "../Components/UserNavbar";
 import UserFooter from "../Components/UserFooter";
 import Rating from "../Components/Rating";
 import { Link } from "react-router-dom"; // Tambahkan import ini
+import { MdLocationOn } from "react-icons/md";
+import { BiArrowFromRight } from "react-icons/bi";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 const DetailProduct = () => {
   const product = {
@@ -44,6 +47,7 @@ const DetailProduct = () => {
       id: 1,
       avatar: "./Avatar/Avatar1.png",
       name: "Sarah",
+      image: "./Product/Basreng.png",
       rating: 5,
       variant: "Pedas Daun Jeruk/250 gr",
       taste: "Enak",
@@ -56,6 +60,7 @@ const DetailProduct = () => {
       id: 2,
       avatar: "./Avatar/Avatar2.png",
       name: "Kelvin",
+      image: "./Product/Basreng-Sajodo.png",
       rating: 4,
       variant: "Original/250 gr",
       taste: "Lezat",
@@ -65,6 +70,131 @@ const DetailProduct = () => {
         "Varian rasa yang saya pilih sangat sesuai dengan selera saya. Pengemasannya juga praktis dan pengiriman sangat cepat. Recommended!",
     },
   ];
+
+  // Data Product
+  const Product = [
+    {
+      productId: 1,
+      productImage: "./Product/Basreng-Sulah-Viral.png",
+      productName: "Basreng Sulah Viral 500gram pedas",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 2,
+      productImage: "./Product/Keripik-Kentang-Balado.png",
+      productName: "Keripik Kentang",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 3,
+      productImage: "./Product/Pisang-Dolar.png",
+      productName: "Pisang Dolar",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 4,
+      productImage: "./Product/Sale-Lidah.png",
+      productName: "Sale Lidah",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 5,
+      productImage: "./Product/Kuping-Mini.png",
+      productName: "Kuping Mini",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 6,
+      productImage: "./Product/Kuping-Gajah.png",
+      productName: "Kuping Gajah",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 7,
+      productImage: "./Product/Kerupuk-Seblak.png",
+      productName: "Kerupuk Seblak",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 8,
+      productImage: "./Product/Cookies-Coklat.png",
+      productName: "Cookies Coklat",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 9,
+      productImage: "./Product/Nastar-Premium.png",
+      productName: "Nastar Premium",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 10,
+      productImage: "./Product/Kue-Kering.png",
+      productName: "Aneka Kue Kering",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 11,
+      productImage: "./Product/GG-SUS.png",
+      productName: "Sus Kering Isi Coklat",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+    {
+      productId: 12,
+      productImage: "./Product/Kacang-Polong.png",
+      productName: "Kacang Polong",
+      originalPrice: 13000,
+      discountPercentage: 12,
+      location: "kab. Bandung",
+      rating: 4.5,
+      soldCount: 50,
+    },
+  ];
+  // End Data Produk
 
   const [selectedImage, setSelectedImage] = useState(0);
   const [cart, setCart] = useState([]);
@@ -194,25 +324,117 @@ const DetailProduct = () => {
           </div>
         </div>
 
-        <div className="mt-4 w-full">
-          <h2 className="text-lg font-medium">Penilaian Produk</h2>
+        <div className="mt-4 w-full py-4">
+          <h2 className="text-2xl font-medium">Penilaian Produk</h2>
           {productReviews.map((review) => (
-            <div key={review.id} className="flex items-start mt-4 w-full">
+            <div key={review.id} className="flex items-start mt-4 w-full py-4">
               <img
                 src={review.avatar}
                 alt={review.name}
                 className="w-12 h-12 rounded-full"
               />
               <div className="ml-4">
-                <p className="text-gray-800 font-semibold">{review.name}</p>
-                <p className="text-gray-600">Varian: {review.variant}</p>
-                <p className="text-gray-600">Rasa: {review.taste}</p>
-                <p className="text-gray-600">Pengemasan: {review.packaging}</p>
-                <p className="text-gray-600">Pengiriman: {review.delivery}</p>
-                <p className="text-gray-600 mt-1">{review.review}</p>
+                <p className="text-gray-800 font-medium text-lg">
+                  {review.name}
+                </p>
+                <div className="text-base">
+                  <p className="text-gray-600">Rasa: {review.taste}</p>
+                  <p className="text-gray-600">
+                    Pengemasan: {review.packaging}
+                  </p>
+                  <p className="text-gray-600">Pengiriman: {review.delivery}</p>
+                  <div className="flex size-52">
+                    <img
+                      src={review.image}
+                      alt={review.name}
+                      className="mr-2"
+                    />
+                    <img
+                      src={review.image}
+                      alt={review.name}
+                      className="mr-2"
+                    />
+                  </div>
+                  <p className="text-gray-600 mt-1">{review.review}</p>
+                </div>
               </div>
             </div>
           ))}
+          <div className="w-full text-end text-base font-Poppins">
+            <Link to="" className=" py-2">
+              Lihat Penilaian Lainnya &gt;
+            </Link>
+          </div>
+        </div>
+        <div className="font-Poppins text-lg px-3 pt-10">
+          <h1 className="font-medium py-4 text-2xl text-center">
+            Rekomendasi Produk Serupa
+          </h1>
+          {/* Product */}
+          <div className="container flex flex-wrap">
+            {Product.map((product) => {
+              // Calculate the discount amount
+              const discountedAmount =
+                (product.discountPercentage / 100) * product.originalPrice;
+
+              // Calculate the price after discount
+              const discountedPrice = product.originalPrice - discountedAmount;
+
+              return (
+                <div
+                  key={product.productId}
+                  className="w-full md:w-1/2 lg:w-1/6 p-1"
+                >
+                  <div className="bg-white border rounded-lg overflow-hidden py-2 hover:bg-white hover:shadow-xl">
+                    <img
+                      src={product.productImage}
+                      alt={product.productName}
+                      className="w-full h-48 object-cover rounded-lg"
+                    />
+                    <div className="p-4">
+                      <h5 className="text-base font-normal text-start line-clamp-2">
+                        {product.productName}
+                      </h5>
+                      <p className="text-black font-medium text-base">
+                        Rp
+                        {discountedPrice.toLocaleString("id-ID")}
+                      </p>
+                      <div className="flex items-center text-xs mt-2">
+                        <p className="text-gray-500 line-through mr-2">
+                          Rp
+                          {product.originalPrice.toLocaleString("id-ID")}
+                        </p>
+                        <p className="text-green-500">
+                          -{product.discountPercentage.toFixed()}% OFF
+                        </p>
+                      </div>
+                      <div className="flex items-center text-xs mt-2">
+                        <MdLocationOn />
+                        <p className="text-gray-400 mr-2">{product.location}</p>
+                      </div>
+                      <div className="flex items-center text-xs mt-2">
+                        <Rating rating={product.rating} totalStars={5} />
+                        <p className="text-gray-400 ml-2">
+                          {product.soldCount} Terjual
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {/* End Product */}
+          {/* See More */}
+          <div className="flex items-center justify-center mt-10">
+            <Link
+              to=""
+              className="px-20 py-2 border border-[#E53935] text-[#E53935] hover:bg-red-50 rounded-full"
+            >
+              Lihat Lainnya
+            </Link>
+          </div>
+          {/* End See More */}
         </div>
       </div>
 
